f1578aac23422ec98b961d4674d30bfa0a72cf85
raeeceip <chiboguchisomu@gmail.com>
2022-11-17 22:03:58 +0100

    initialization

--- main.py
+++ main.py
@@ -1,1 +1,32 @@
-#start implementation 
+# library imports
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+
+# module imports
+from .routes import blog_content,users, auth, password_reset
+
+# initialize an app
+app = FastAPI()
+
+# Handle CORS protection
+origins = ["*"]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+
+# register all the router endpoint
+app.include_router(blog_content.router)
+app.include_router(users.router)
+app.include_router(auth.router)
+app.include_router(password_reset.router)
+
+
+@app.get("/")
+def get():
+    return {"msg": "Hello world"}
